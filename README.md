a7eb5ba5555012fab8234c7f7f5f4b5f0f82cce4
danscott2150 <danscott2150@gmail.com>
2018-08-11 15:40:57 -0400

    Minor code cleanup + updated readme

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
+- For use with StateDroneLaw.com
 - Goal: Use OpenStates.org API to keep track of all drone-related laws across all 50 states
 
-- Ideally, run program =>
-- Program makes API call to OpenStates.org, searches all 50 states for laws
-  with the keywords "drone", "drones", or "unmanned" =>
-- Returns JSON data of all relevant bills & their info =>
-- Saves data to a spreadsheet? Or a Database? =>
-- Checks updated data against previous data, alerts me to any new bills or updates
+- Ideal end result:
+--- Program makes API call to OpenStates.org, searches all 50 states for laws with keywords "drone" or "unmanned",
+--- Returns JSON data containing all relevant bills & related info,
+--- Saves JSON data into .CSV format, to create an excel spreadsheet
+--- ##Figure out a way to check new data vs data from last time the program was run? i.e. auto-alert me to new or changed bills?
+--- ##Figure out a way to somehow integrate directly with WordPress?
 
-- Pie in the sky: somehow integrate directly into WordPress
+=====8.11=====
+  - Core functionality seems to be working. Feel like the code is super messy, will try to re-factor at a later date.
+  - Currently, making an API call for "unmanned" keyword and putting the results into an 'allBills' array via a promise. Then,
+      second API call for "drone" keyword adds results into same 'allBills' array. Accomplishing this via forEach loop to iterate over each item returned in JSON. Tried using allBills.push() with the JSON data as a whole, but this resulted in nested arrays. Tried using lodash's .merge() function but that didn't seem to work, or maybe I just didn't understand
+      how to use it correctly.
+  - To-do's:
+      -- Right now, .csv data includes brackets & curly brackets for the "sources" and "Action Dates" fields. Find way to remove these when printing to .csv? Also 'Action Dates' includes two name/value pairs, find way to break out into two separate columns?
+      -- In the event that a bill uses BOTH keywords "drone" and "unmanned", we end up with a duplicate value in the data.
+      -- Break out API key into env variable, so it's not public
+  - Next project:
+      -- Is there a way to compare the .csv data to the .csv export from the previous time the program was run? i.e. if I run it for the first time, I get a .csv file back with ~250 bills. If I then run the program again a week later, it shows/alerts me to the specific bills that are either new or have changed since the original run. OpenStates gives each bill a unique ID, so it should be possible to check only for IDs that don't exist. For bills that have changed, should be possible to compare the "last action date" fields?
+      -- Is there a way to somehow integrate with WordPress? Or at least get certain values to auto-fill into ACF fields?
--- app.js
+++ app.js
@@ -1,135 +1,81 @@
+///// ***** OpenStates.org API Documentation: ***** /////
+//http://docs.openstates.org/en/latest/api/index.html
 
-// const request = require("request");
+// const request = require("request"); //can uninstall?
+// const _ = require('lodash'); //can uninstall?
+// const argv = require('yargs').argv;  //can uninstall?
+
+
 const axios = require("axios");
-const _ = require('lodash');
-const argv = require('yargs').argv;
-
 const fs = require('fs');
 const json2csvParser = require('json2csv').Parser;
 
-//OpenStates.org API Documentation:
-//http://docs.openstates.org/en/latest/api/index.html
-
 var openStatesKey = "5894d791-dfab-4a95-9e60-2613f8d9ea90";
 var openStatesURL = "https://openstates.org/api/v1/";
-var fieldsTemp = "bill_id,title,state,session,action_dates.last,action_dates.signed,sources"
+var fields = "bill_id,title,state,session,action_dates.last,action_dates.signed,sources"  //passed directly into API URL
 
-//session,action_dates.last,action_dates.signed,sources
-// var fields = ['bill_id','title','session','action_dates.last','action_dates.signed','sources'];
-const searchFields = ['bill_id','title','state'];
-//'session','action_dates.last','action_dates.signed','sources'
-// const opts = { searchFields };
-
-var currentBills = [];
-
-var queries = ["unmanned", "drones", "drone"];
-var allBills = [];
-// var states = ["NY", "MA", "NJ"];
+// var queries = ["unmanned", "drones", "drone"]; //for future use DRY code? Loop over each query and make an API?
+// var states = ["NY", "MA", "NJ"]; //(used for testing purposes)
 var states = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE",
 "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA",
 "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH",
 "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD",
 "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
 
-var billData;
+var allBills = [];
 
-var tempState = "nj";
-
+//Loop through each state and make API calls
 states.forEach(function(state){
   function runUnmannedCall(){
-    return axios.get(`https://openstates.org/api/v1/bills/?state=${state}&search_window=term&q=unmanned&fields=${fieldsTemp}&apikey=${openStatesKey}`);
+    //First, run API call for the keyword "Unmanned"
+    // -- need to run calls for both "unmanned" and "drone" because most bills only refer to one or the other exclusively
+    return axios.get(`https://openstates.org/api/v1/bills/?state=${state}&search_window=term&q=unmanned&fields=${fields}&apikey=${openStatesKey}`);
+    //'search_window=term' limits results to only the state's current legislative term/session/year, prevents old data from being pulled
   }
     runUnmannedCall().then((response) => {
+
+      //With the data returned from the API call, run a for-loop
+      //to cycle through each object and add it to the allBills array.
+
+      //Originally I used allBills.push(response.data), which was much shorter & cleaner, but that returned
+      //an array into an array, thus turning allBills into an array of arrays containing objects.
+      //This created problems when the second set of data (from the API call below) was also added to
+      //allBills, and caused the csv-parser to become buggy. There's probably an easier/cleaner way to solve
+      //this problem, but this works for now
       for(var i=0; i<response.data.length; i++){
         allBills.push(response.data[i]);
       }
-      // console.log(allBills);
-      var unmannedResults = response.data[0];  //SAVED AS A JSON OBJECT
-      // console.log("Unmanned: ", unmannedResults);
-      // console.log(typeof(unmannedResults));
-      // console.log(response.data[]);
+
       return new Promise ((resolve, reject) => {
-          // allBills.push(unmannedResults);
-          // console.log("UR: ", allBills);
           resolve(allBills);
-        }).then((response) => {   //AllBills currently JSON object
-          return axios.get(`https://openstates.org/api/v1/bills/?state=${state}&search_window=term&q=drone&fields=${fieldsTemp}&apikey=${openStatesKey}`);
+
         }).then((response) => {
+          //Run second API call for the same state, using 'drone' keyword
+          return axios.get(`https://openstates.org/api/v1/bills/?state=${state}&search_window=term&q=drone&fields=${fields}&apikey=${openStatesKey}`);
+
+        }).then((response) => {
+          //Loop through responses and add to allBills array
           for(var i=0; i<response.data.length; i++){
             allBills.push(response.data[i]);
           }
-          // console.log(allBills);
-          // var droneResults = response.data;
-          // console.log("DR: ", typeof(droneResults));
-          // console.log("Drone Results:", droneResults);
-          // var newAll = _.concat(allBills, droneResults);
-          // console.log(newAll);
-          // _.merge()
-          // _.merge(droneResults, unmannedResults);
-          // console.log(unmannedResults);
-          // allBills.push(droneResults);
-          // console.log(allBills);
+
           return new Promise((resolve, reject) => {
             resolve(allBills);
-          }).then((response) => {
-            var tempVar = allBills;
-            // console.log(tempVar);
-            // console.log(tempVar);
-            // console.log(allBills[0]);
-            // console.log(allBills);
-            // let fields = ['title', 'state', 'id', 'bill_id'];
-            // var parserOpts = { fields };
-            // console.log(parserOpts);
-            const parser = new json2csvParser();
-            // console.log(parser);
-            const csv = parser.parse(tempVar);
-            // console.log(csv);
 
-            // var data = JSON.stringify(allBills);
-            // var data2 = JSON.parse(data);
-            // console.log(csv);
+          }).then((response) => {
+
+            //Parse data from allBills into .csv format
+            //uses 'json2csv' module: https://www.npmjs.com/package/json2csv
+            var csvData = allBills;
+            const parser = new json2csvParser();
+            const csv = parser.parse(csvData);
+
+            //Save .CSV formatted data into external file
             fs.writeFile('data.csv', csv, (err) => {
               if(err){console.log(err)}
             });
 
-          });
-        });
-    });
-
-
-
-
-  });
-
-
-//
-// request(`https://openstates.org/api/v1/bills/?state=nj&search_window=term&q=unmanned&fields=${fields}&apikey=${openStatesKey}`,
-//   (error, response, body) => {
-//     if(error){
-//       console.log(error);
-//     } else {
-//       //Loop through each object
-//       let bills = JSON.parse(body);
-//
-//       const parser = new json2csvParser(opts);
-//       const csv = parser.parse(bills);
-//       // fs.writeFile('data.csv', csv, (err) => {
-//       //   if (err){console.log(err);}
-//       // });
-//       // return bills;
-//       bills.forEach(function(bill){
-//         return new Promise((resolve, reject) => {
-//           if(bill){
-//             resolve(bill);
-//           }
-//         });
-//         currentBills.push(bill);
-//       });
-//       console.log(currentBills);
-//
-//     }
-// }).then((response) => {
-//   currentBills.push(response);
-// }).then((response) => {
-//   console.log(currentBills);
-// });
+          }); //.then => Parse to CSV
+        }); //.then => Add 'drone' API call to allBills
+    }); //initial 'unmanned' API call
+  }); //forEach(states)
